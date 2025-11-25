2bd8ce77eb22f37228debebea354e20fe220cd9b
inlinefun <inlinefun@proton.me>
2025-11-25 16:57:25 +0530

    update(zsh): aliases

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -2,10 +2,11 @@ alias l='ls --color -AlhGg'
 alias ls='ls --color'
 
 alias mv='mv -i'
-alias rm='rm -i'
+alias rm='rm'
 alias md='mkdir'
 
 alias c='clear'
 alias q='exit'
+alias :q='exit'
 
 alias ..='cd ..'
