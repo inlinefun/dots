9cb5d379214d9d40a7e86648e9456531697579b7
inlinefun <inlinefun@proton.me>
2025-12-27 17:07:08 +0530

    add: zsh

---
+++ dot_config/zsh/aliases.zsh
@@ -0,0 +1,13 @@
+alias l='ls --color -AlhGg'
+alias ls='ls --color'
+alias a='ls --color'
+
+alias mv='mv -i'
+alias rm='rm'
+alias md='mkdir'
+
+alias c='clear'
+alias q='exit'
+alias :q='exit'
+
+alias ..='cd ..'
---
+++ dot_config/zsh/binds.zsh
@@ -0,0 +1,5 @@
+bindkey '^[[1;5D' backward-word
+bindkey '^[[1;5C' forward-word
+bindkey '^[[3~' delete-char
+bindkey '^[[3;5~' kill-word
+bindkey '^H' backward-kill-word
